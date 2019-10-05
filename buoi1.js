3f6e3bc1295b58f456b3dbe211b676838418265d
phamtanphat <phatdroid94@gmail.com>
2019-10-05 02:14:50 -0700

    Khai bao bien va kieu du lieu

---
+++ buoi1.js
@@ -0,0 +1,30 @@
+// console.log("Hello");
+// console.log("Xin chao");
+// console.log("Lop react native 0510");
+// mo moi truong thuc thi  : ctrl + ~
+// nodemon : tu dong thuc thi lai file khi co thay doi
+// thoat khoi trang thai nodemon : ctrl + C
+
+// 1 : Khai bao bien
+// var  const  let
+// var a = "Pham tan phat"
+// a = 10
+// let b = true
+// b = 15
+// // const not assignment
+// const c = "1000"
+// c = 1000
+// console.log(c);
+//2 : Kieu du lieu 
+
+// primities :
+    1 - String
+    2 - Boolean
+    3 - Numerber
+    // special type
+    4 - null
+    5 - undefined
+    6 - symbol
+// object
+    1 - Object
+    2 - Array
