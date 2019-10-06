447b3e09b1a7a21ed5333da0e26c4e582d6480b7
phamtanphat <phatdroid94@gmail.com>
2019-10-05 21:15:13 -0700

    bai tap 2 for

--- buoi2.js
+++ buoi2.js
@@ -170,3 +170,24 @@
 // for(var i = array.length - 1 ; i > -1 ; i--){
 //     console.log(array[i])
 // }
+// bai tap 2
+// Ví dụ
+var a = [1, 2, 3, 4, 5];
+var b = [10, 20, 30];
+/**
+ * Sử dụng vòng lặp for để tính tích của các phần tử của array a với các phần tử của array b Kết quả mong muốn:
+ * 10
+ * 20
+ * 30
+ * ...
+ * 20
+ * 40
+ * 60
+ * ...
+ * Gợi ý: Dùng nested for loops (các vòng lặp lồng nhau)
+ */
+for(var i = 0 ; i < b.length ; i++){
+    for(var y = 0 ; y < a.length ; y++){
+        console.log(b[i] * a[y]);
+    }
+}
