8fa16378aea3138dd0161f4f703305a55793c64f
phamtanphat <phatdroid94@gmail.com>
2019-10-05 20:37:48 -0700

    bai tap Object method

--- buoi2.js
+++ buoi2.js
@@ -70,29 +70,63 @@
     // b %= 2 
     // console.log(b)
 //5 : function 
-// function dientichTamGiac(a , h){
-//     var s = a * h / 2
-    // return hoac khong co return thi method tra ve gia tri undefined
-//     return s
+    // function dientichTamGiac(a , h){
+    //     var s = a * h / 2
+        // return hoac khong co return thi method tra ve gia tri undefined
+    //     return s
+    // }
+    // console.log(dientichTamGiac(5 , 10))
+
+
+    // Bài tập: Viết hàm tính thể tích khối hộp.
+    // Nhận vào các tham số như dưới, trả về thể tích của khối hộp.
+    /**
+     * Tính thể tích khối hộp
+     * https://www.wikihow.com/Calculate-Volume-of-a-Box
+     * @param {Number} w Chiều rộng
+     * @param {Number} h Chiều cao
+     * @param {Number} l Chiều dài
+     * @returns {Number} Diện tích
+
+    function getBoxVolume(w, h, l) {
+        var v = w * h * l
+        return v
+    }
+    
+    console.log(getBoxVolume(10, 20, 30)); // 6000
+    console.log(getBoxVolume(4, 5, 6)); // 120
+    */
+//6 : Object method
+// var khoapham = {
+//     name : 'Khoa Pham',
+//     age : 35,
+//     info : function(){
+//         console.log("Ten : " + this.name + " Tuoi : " + this.age)
+//     }
 // }
-// console.log(dientichTamGiac(5 , 10))
-
-
-// Bài tập: Viết hàm tính thể tích khối hộp.
-// Nhận vào các tham số như dưới, trả về thể tích của khối hộp.
+// khoapham.info()
 /**
- * Tính thể tích khối hộp
- * https://www.wikihow.com/Calculate-Volume-of-a-Box
- * @param {Number} w Chiều rộng
- * @param {Number} h Chiều cao
- * @param {Number} l Chiều dài
- * @returns {Number} Diện tích
-
-function getBoxVolume(w, h, l) {
-    var v = w * h * l
-    return v
-}
-  
-console.log(getBoxVolume(10, 20, 30)); // 6000
-console.log(getBoxVolume(4, 5, 6)); // 120
+ * Khai báo biến `rectangle` là 1 object gồm có các property:
+ * - width: chiều rộng, là 1 số bất kì
+ * - height: chiều cao, là 1 số bất kì
+ * Viết các method:
+ * - getWidth: trả về chiều rộng
+ * - getHeight: trả về chiều cao
+ * - getArea: trả về diện tích
  */
+const rectangle = {
+    width : 10 ,
+    height : 15 ,
+    getWidth : function(){
+        return this.width
+    },
+    getHeight : function(){
+        return this.height
+    },
+    getArea : function(){
+        return this.width * this.height 
+    }
+}
+console.log(rectangle.getWidth())
+console.log(rectangle.getHeight())
+console.log(rectangle.getArea())
