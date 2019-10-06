2c4f90145b1cdd97f473609354ca7b5fd0f95f15
phamtanphat <phatdroid94@gmail.com>
2019-10-05 22:03:12 -0700

    bai tap 2 for in for of

--- buoi2.js
+++ buoi2.js
@@ -227,8 +227,66 @@
  * Sử dụng vòng lặp for...of để tính tích của các phần tử trong một array gồm các số
  */
 
-function multiply(arr) {
+// function multiply(arr) {
+//     var tich = 1
+//     for(var i of arr){
+//         tich *= i
+//     }
+//     return tich
+// }
+
+// console.log(multiply([2, 3, 4])); // expect: 24
+//Ví dụ
+
+/**
+ * Sử dụng vòng lặp for...in để in ra tất cả các key trong object sau, in ra cả các key của các nested object (object con lồng bên trong object lớn)
+ * 
+ */
+
+var apartment = {
+  bedroom: {
+    area: 20,
+    bed: {
+      type: 'twin-bed',
+      price: 100
+    }
+  }
+};
+
+function getkey(object){
+    for(var key in object){
+    
+        console.log(key)
+        for(var keybedroom in object[key]){
+            console.log(keybedroom)
+            for(var keybed in object[key][keybedroom]){
+                console.log(keybed)
+            }
+        }
+    }
 
 }
 
-console.log(multiply([2, 3, 4])); // expect: 24
+function getkey(object){
+    var output = ''
+    for(var key in object){
+        // key la 1 object
+        output = key
+        console.log(output)
+        if(typeof object[output] === 'object'){
+            output = getkey(object[key])
+        }
+    }
+
+}
+getkey(apartment)
+
+/**
+ * Kết quả mong muốn:
+ * bedroom
+ * area
+ * bed
+ * type
+ * price
+ * Chú ý: không cần hiển thị ra đúng thứ tự như trên
+ */
