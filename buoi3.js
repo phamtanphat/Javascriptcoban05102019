9d79ed9393a11c539a23b8dece5b07f3bdb5326b
phamtanphat <phatdroid94@gmail.com>
2019-10-12 00:06:40 -0700

    array map

--- buoi3.js
+++ buoi3.js
@@ -32,69 +32,76 @@
 
     // console.log(first([1, 2, 3], 2)); // expect [1, 2]
 //11 : Callback la gia tri
-// var giangvien = {
-//     name : 'Pham Tan Phat',
-//     thoigianday : function(onFinish){
-//         console.log("Trua thu bay va sang chu nhat")
-//         onFinish()
-//     }
-// }
-// function finish(){
-//     console.log("Ket thuc buoi hoc")
-// }
-// var finish = function(){
-// }
-// giangvien.thoigianday(function(){
-//     console.log("Ket thuc")
-// })
-// var maylocnuoc = {
-//     name : 'May loc nuoc viet nam',
-//     locnuoc : function(onFinish){
-//         console.log("Dang trong trinh loc nuoc")
-//         onFinish()
-//     }
-// }
-// function lochoantat(){
-//     console.log("Loc hoan tat")
-// }
-// maylocnuoc.locnuoc(console.log())
-// Bai tap 1
-/**
- * 1. Viết hàm double nhận vào 1 số bất kì, hiển thị ra số đó nhân đôi
- * 2. Viết hàm sumAndDo nhận vào 1 array và 1 callback function. Hàm sumAndDo làm nhiệm vụ tính tổng array đó, sau đó gọi callback function với tham số là kết quả tổng vừa tính
- */
-// function double(num) {
-//     console.log(num * 2)
-// }
-// function sumAndDo(nums, callback) {
-//     var sum = 0
-//     for(var value of nums){
-//         sum += value
-//     }
-//     callback(sum)
-// }
+    // var giangvien = {
+    //     name : 'Pham Tan Phat',
+    //     thoigianday : function(onFinish){
+    //         console.log("Trua thu bay va sang chu nhat")
+    //         onFinish()
+    //     }
+    // }
+    // function finish(){
+    //     console.log("Ket thuc buoi hoc")
+    // }
+    // var finish = function(){
+    // }
+    // giangvien.thoigianday(function(){
+    //     console.log("Ket thuc")
+    // })
+    // var maylocnuoc = {
+    //     name : 'May loc nuoc viet nam',
+    //     locnuoc : function(onFinish){
+    //         console.log("Dang trong trinh loc nuoc")
+    //         onFinish()
+    //     }
+    // }
+    // function lochoantat(){
+    //     console.log("Loc hoan tat")
+    // }
+    // maylocnuoc.locnuoc(console.log())
+    // Bai tap 1
+    /**
+     * 1. Viết hàm double nhận vào 1 số bất kì, hiển thị ra số đó nhân đôi
+     * 2. Viết hàm sumAndDo nhận vào 1 array và 1 callback function. Hàm sumAndDo làm nhiệm vụ tính tổng array đó, sau đó gọi callback function với tham số là kết quả tổng vừa tính
+     */
+    // function double(num) {
+    //     console.log(num * 2)
+    // }
+    // function sumAndDo(nums, callback) {
+    //     var sum = 0
+    //     for(var value of nums){
+    //         sum += value
+    //     }
+    //     callback(sum)
+    // }
 
-// sumAndDo([1, 2, 3], double);
-//Bai tap 2 :
-/**
- * 1. Viết hàm transform nhận vào 1 array gồm các số và 1 function callback. Nội dung của hàm bao gồm:
- * - Khai báo 1 biến result có giá trị là 1 empty array
- * - Lặp qua từng phần tử trong array, với mỗi phần tử, gọi hàm callback và truyền vào phần tử đó, được kết quả bao nhiêu thì push vào array khai báo ở trên
- * - Trả về result array
- */
-// function transform(numbers, callback) {
-//     // Viết nội dung hàm ở đây 
-//         var arrayResult = []
-//         for(var value of numbers){
-//             arrayResult.push(callback(value))
-//         }
-//         return arrayResult
-//     }
-//     function double(num) {
-//       return num * 2;
-//     }
-    
-//     var output = transform([2, 4, 6], double);
-//     console.log(output);
-    // Expect: [4, 8, 12]
-    
+    // sumAndDo([1, 2, 3], double);
+    //Bai tap 2 :
+    /**
+     * 1. Viết hàm transform nhận vào 1 array gồm các số và 1 function callback. Nội dung của hàm bao gồm:
+     * - Khai báo 1 biến result có giá trị là 1 empty array
+     * - Lặp qua từng phần tử trong array, với mỗi phần tử, gọi hàm callback và truyền vào phần tử đó, được kết quả bao nhiêu thì push vào array khai báo ở trên
+     * - Trả về result array
+     */
+    // function transform(numbers, callback) {
+    //     // Viết nội dung hàm ở đây 
+    //         var arrayResult = []
+    //         for(var value of numbers){
+    //             arrayResult.push(callback(value))
+    //         }
+    //         return arrayResult
+    //     }
+    //     function double(num) {
+    //       return num * 2;
+    //     }
+        
+    //     var output = transform([2, 4, 6], double);
+    //     console.log(output);
+        // Expect: [4, 8, 12]
+//12 : Ham map
+
+// var numbers = [1,2,3,4,5]
+
+// var newNumbers = numbers.map(function(num){
+//     return num * 2
+// })
+// console.log(newNumbers)
