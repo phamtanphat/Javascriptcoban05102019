3c5335d1658ecab04d8afd0ae64ba9f54d0b3ad0
phamtanphat <phatdroid94@gmail.com>
2019-10-12 00:16:43 -0700

    btap1 : map method

--- buoi3.js
+++ buoi3.js
@@ -105,3 +105,23 @@
 //     return num * 2
 // })
 // console.log(newNumbers)
+
+//bai tap 1 :
+
+/**
+ * 1. Viết hàm tripple nhận vào 1 số và trả về số đó nhân 3
+ * 2. Sử dụng map method và hàm tripple để nhân 3 các phần 
+ * tử trong mảng dưới đây
+ */
+// Khai báo tripple function ở đây
+function tripple(num){
+    return num * 3
+}
+var numbers = [10, 5, 8];
+var newNumbers = numbers.map(function(num){
+    return tripple(num)
+})
+
+console.log(newNumbers)
+
+// Gợi ý: numbers.map... (tự viết tiếp)
