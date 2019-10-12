f5b1d738ec234fb681c20ec9f03dad63d0e0e5b6
phamtanphat <phatdroid94@gmail.com>
2019-10-11 23:02:35 -0700

    array method

---
+++ buoi3.js
@@ -0,0 +1,16 @@
+// 10 : Array method
+//Them va xoa gia tri trong mang
+// array.push : them 1 phan tu vao cuoi mang - tra ve kich thuoc mang
+// array.pop : xoa 1 phan tu cuoi - tra ve phan tu bi xoa
+// array.shift : xoa phan tu dau tien trong mang - tra ve phan tu bi xoa
+// array.unshift : them 1 phan tu vao dau mang - tra ve kich thuoc mang
+// array.splice : 
+        // + Them gia tri vao vi tri nao arrayNumber.splice(vitri , 0 , giatrithemvao  )
+        // - Xoa gia tri o vi tri arrayNumber.splice(vitri, soluongphantu  )
+var arrayNumber = [1,2,3,4,5]
+// arrayNumber.push(6)
+// arrayNumber.pop
+// arrayNumber.shift()
+// // arrayNumber.unshift(0)
+// arrayNumber.splice(1 , 1  )
+console.log(arrayNumber)
