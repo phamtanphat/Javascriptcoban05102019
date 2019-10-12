e269a66c6280d20f6dd122a2930bd636ba55cafd
phamtanphat <phatdroid94@gmail.com>
2019-10-12 02:05:28 -0700

    bai 2 : reduce method

--- buoi3.js
+++ buoi3.js
@@ -258,38 +258,46 @@
 // console.log(sum)
 // Bai tap 1
 // Trả về số lượng người đã vote
-function totalVotes(arr) {
-    var tong = 0
-    var count = arr.reduce(function(total , currentvalue){
-        if(typeof total === 'object'){
-            for(var key in total){
-                if(key === 'voted'){
-                    if( total[key] == true){
-                       tong += 1 
-                    }
-                }
-            }
-        }
-        if(currentvalue.voted == true){
-            tong += 1
-        }
-        return tong
-    })
-    return count
-}
+// function totalVotes(arr) {
 
-var voters = [
-  {name:'Bob' , age: 30, voted: true},
-  {name:'Jake' , age: 32, voted: true},
-  {name:'Kate' , age: 25, voted: false},
-  {name:'Sam' , age: 20, voted: false},
-  {name:'Phil' , age: 21, voted: true},
-  {name:'Ed' , age:55, voted:true},
-  {name:'Tami' , age: 54, voted:true},
-  {name: 'Mary', age: 31, voted: false},
-  {name: 'Becky', age: 43, voted: false},
-  {name: 'Joey', age: 41, voted: true},
-  {name: 'Jeff', age: 30, voted: true},
-  {name: 'Zack', age: 19, voted: false}
-];
-console.log(totalVotes(voters)) // 7
+//     var count = arr.reduce(function(total , currentvalue){
+//         if(currentvalue.voted == true){
+//             total++
+//         }
+//         return total
+//     },0)
+//     return count
+// }
+
+// var voters = [
+//   {name:'Bob' , age: 30, voted: true},
+//   {name:'Jake' , age: 32, voted: true},
+//   {name:'Kate' , age: 25, voted: false},
+//   {name:'Sam' , age: 20, voted: false},
+//   {name:'Phil' , age: 21, voted: true},
+//   {name:'Ed' , age:55, voted:true},
+//   {name:'Tami' , age: 54, voted:true},
+//   {name: 'Mary', age: 31, voted: false},
+//   {name: 'Becky', age: 43, voted: false},
+//   {name: 'Joey', age: 41, voted: true},
+//   {name: 'Jeff', age: 30, voted: true},
+//   {name: 'Zack', age: 19, voted: false}
+// ];
+// console.log(totalVotes(voters)) // 7
+
+// bai tap 2
+// Tính tổng giá trị các món bên dưới
+// function shoppingSpree(arr) {
+//     return arr.reduce(function(total , currentvalue){
+//         return total + currentvalue.price
+//     },0)
+// }
+
+// var wishlist = [
+//   { title: "Tesla Model S", price: 90000 },
+//   { title: "4 carat diamond ring", price: 45000 },
+//   { title: "Fancy hacky Sack", price: 5 },
+//   { title: "Gold fidgit spinner", price: 2000 },
+//   { title: "A second Tesla Model S", price: 90000 }
+// ];
+// console.log(shoppingSpree(wishlist))// 227005
