c9c3cb7bc8a0290af11e9c88ff2732704a23d55e
phamtanphat <phatdroid94@gmail.com>
2019-10-11 23:58:19 -0700

    btap 2 : callback la gia tri

--- buoi3.js
+++ buoi3.js
@@ -58,3 +58,43 @@
 //     console.log("Loc hoan tat")
 // }
 // maylocnuoc.locnuoc(console.log())
+// Bai tap 1
+/**
+ * 1. Viết hàm double nhận vào 1 số bất kì, hiển thị ra số đó nhân đôi
+ * 2. Viết hàm sumAndDo nhận vào 1 array và 1 callback function. Hàm sumAndDo làm nhiệm vụ tính tổng array đó, sau đó gọi callback function với tham số là kết quả tổng vừa tính
+ */
+// function double(num) {
+//     console.log(num * 2)
+// }
+// function sumAndDo(nums, callback) {
+//     var sum = 0
+//     for(var value of nums){
+//         sum += value
+//     }
+//     callback(sum)
+// }
+
+// sumAndDo([1, 2, 3], double);
+//Bai tap 2 :
+/**
+ * 1. Viết hàm transform nhận vào 1 array gồm các số và 1 function callback. Nội dung của hàm bao gồm:
+ * - Khai báo 1 biến result có giá trị là 1 empty array
+ * - Lặp qua từng phần tử trong array, với mỗi phần tử, gọi hàm callback và truyền vào phần tử đó, được kết quả bao nhiêu thì push vào array khai báo ở trên
+ * - Trả về result array
+ */
+// function transform(numbers, callback) {
+//     // Viết nội dung hàm ở đây 
+//         var arrayResult = []
+//         for(var value of numbers){
+//             arrayResult.push(callback(value))
+//         }
+//         return arrayResult
+//     }
+//     function double(num) {
+//       return num * 2;
+//     }
+    
+//     var output = transform([2, 4, 6], double);
+//     console.log(output);
+    // Expect: [4, 8, 12]
+    
