8444e9556b98119cfa7d2fa5b1be2e2a48a1fb76
phamtanphat <phatdroid94@gmail.com>
2019-10-12 21:39:36 -0700

    diff arror function vs function

--- buoi4.js
+++ buoi4.js
@@ -312,21 +312,35 @@
 
     // run(mouse.run.bind(mouse))
 // 24 : arrow function (fat arrow)
-// const arraynumber = [1,2,3,4,5,6,7,8,9,10]
-// const newArray = arraynumber.map( (item , index)  => item * 2)
-// console.log(newArray)
-/**
+    // const arraynumber = [1,2,3,4,5,6,7,8,9,10]
+    // const newArray = arraynumber.map( (item , index)  => item * 2)
+    // console.log(newArray)
+    /**
      * 1. Viết hàm tripple nhận vào 1 số và trả về số đó nhân 3
      * 2. Sử dụng map method và hàm tripple để nhân 3 các phần 
      * tử trong mảng dưới đây
      */
     // Khai báo tripple function ở đây
-    function tripple(num){
-        return num * 3
-    }
-    var numbers = [10, 5, 8];
-    var newNumbers = numbers.map()
-    console.log(newNumbers)
+    // function tripple(num){
+    //     return num * 3
+    // }
+    // var numbers = [10, 5, 8];
+    // // [30 , 15 , 24]
+    // var newNumbers = numbers.map(item => tripple(item))
+    // console.log(newNumbers)
+// 25 : arrow function va function
 
+const a = {
+    name : 'A',
+    run : function(){
+        const that = this
+        console.log(this.name + ' running')
+        var run2 =  function() {
+            console.log(that.name + ' running second')
+        }
+        run2()
+    }
+}
+a.run()
 
 
