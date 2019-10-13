e1cb4374c410f4b7761574eb970fb8873dbb2d0b
phamtanphat <phatdroid94@gmail.com>
2019-10-12 18:10:14 -0700

    bai 2 : array tong hop

--- buoi3.js
+++ buoi3.js
@@ -102,7 +102,7 @@
     // var numbers = [1,2,3,4,5]
 
     // var newNumbers = numbers.map(function(num){
-    //     return num * 2
+    //     return false
     // })
     // console.log(newNumbers)
 
@@ -327,30 +327,30 @@
   ];
 */
 
-var listProducts = [
- {id: 1, categoryId: 1, name: 'Tivi'},
- {id: 2, categoryId: 1, name: 'Tủ lạnh'},
- {id: 3, categoryId: 3, name: 'Ghế sofa'},
- {id: 4, categoryId: 1, name: 'Máy giặt'},
- {id: 5, categoryId: 2, name: 'Chén bát'},
- {id: 6, categoryId: 2, name: 'Nồi cơm điện'},
- {id: 7, categoryId: 3, name: 'Cửa kính'},
- {id: 8, categoryId: 1, name: 'Điều hoà'},
- {id: 9, categoryId: 3, name: 'Bàn tròn'},
- {id: 10, categoryId: 2, name: 'Lò vi sóng'},
-]
+// var listProducts = [
+//  {id: 1, categoryId: 1, name: 'Tivi'},
+//  {id: 2, categoryId: 1, name: 'Tủ lạnh'},
+//  {id: 3, categoryId: 3, name: 'Ghế sofa'},
+//  {id: 4, categoryId: 1, name: 'Máy giặt'},
+//  {id: 5, categoryId: 2, name: 'Chén bát'},
+//  {id: 6, categoryId: 2, name: 'Nồi cơm điện'},
+//  {id: 7, categoryId: 3, name: 'Cửa kính'},
+//  {id: 8, categoryId: 1, name: 'Điều hoà'},
+//  {id: 9, categoryId: 3, name: 'Bàn tròn'},
+//  {id: 10, categoryId: 2, name: 'Lò vi sóng'},
+// ]
 
-function filterProductsByCategoryId(products, categoryId) {
-  // your code here!
+// function filterProductsByCategoryId(products, categoryId) {
+//   // your code here!
 
-    var arrayFilterCategoryId = products.filter(function(item){
-        return item.categoryId == categoryId
-    })
-    return arrayFilterCategoryId.map(function(item){
-        delete item.categoryId
-        return item
-    })
-}
-console.log(filterProductsByCategoryId(listProducts,3))
+//     var arrayFilterCategoryId = products.filter(function(item){
+//         return item.categoryId == categoryId
+//     })
+//     return arrayFilterCategoryId.map(function(item){
+//         delete item.categoryId
+//         return item
+//     })
+// }
+// console.log(filterProductsByCategoryId(listProducts,3))
 
 
---
+++ buoi4.js
@@ -0,0 +1,23 @@
+//Bai tap
+/**
+ * Viêt hàm có sử dụng method filter để loại bỏ những phần tử bị lặp lại trong mảng:
+ * Tìm hiểu thêm indexOf:
+ * https://developer.mozilla.org/vi/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+ * Example:
+ * removeDuplicate([1, 1, 2, 3, 3]) // [1, 2, 3]
+ * 
+*/
+
+function removeDuplicate(array = []) {
+  // write code here...
+    var arrayOutput = array.filter(function(item , index ,arr){
+        // console.log(item , index)
+        if(index == arr.indexOf(item)){
+            return true
+        }
+        return false
+    })
+    return arrayOutput
+    
+}
+console.log(removeDuplicate([1, 1, 2, 3, 3 ,4,4,5]))
