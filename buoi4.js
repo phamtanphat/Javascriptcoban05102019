21545fffdc24ea32b297d6f7054de0c2033adb1b
phamtanphat <phatdroid94@gmail.com>
2019-10-12 19:08:34 -0700

    bai tap 1 : object handle

--- buoi4.js
+++ buoi4.js
@@ -23,49 +23,111 @@
     // console.log(removeDuplicate([1, 1, 2, 3, 3 ,4,4,5]))
 // 16 : Math 
 
-// Math.round() : lam tron gia tri 
-// Math.ceil() : lam tron len
-// Math.floor() : lam tron xuong
-// Math.abs() : tri tuyet doi
-// Math.max() : tim ra gia tri lon nhat
-// Math.min() : tim ra gia tri nho nhat
-// Math.random() : tra 0 ~ 1
-// a = Math.random() * 10
-// console.log(Math.round(a))
-// bai tap 1 :
-// Dùng hàm powerup tính bình phương các số chia hết cho 2 trong mảng truyền vào
-// [1,3,2,5,6] => [1,3,4,5,36]
-// function powerup(arr) {
-//   // your code here!
-//   return arr.map(function(item){
-//       if(item % 2 == 0) return Math.pow(item , 2)
-//       return item
-//   })
+    // Math.round() : lam tron gia tri 
+    // Math.ceil() : lam tron len
+    // Math.floor() : lam tron xuong
+    // Math.abs() : tri tuyet doi
+    // Math.max() : tim ra gia tri lon nhat
+    // Math.min() : tim ra gia tri nho nhat
+    // Math.random() : tra 0 ~ 1
+    // a = Math.random() * 10
+    // console.log(Math.round(a))
+    // bai tap 1 :
+    // Dùng hàm powerup tính bình phương các số chia hết cho 2 trong mảng truyền vào
+    // [1,3,2,5,6] => [1,3,4,5,36]
+    // function powerup(arr) {
+    //   // your code here!
+    //   return arr.map(function(item){
+    //       if(item % 2 == 0) return Math.pow(item , 2)
+    //       return item
+    //   })
+    // }
+    // console.log(powerup([1,3,2,5,6]))
+    //Bai tap 2 
+    // * 
+    //   Dùng hàm average tính điểm trung bình 3 môn của một học sinh.
+    //   Làm tròn điểm trung bình
+    // */
+
+    // function average(arr) {
+    //     return Math.round(arr.reduce(function(total , currentvalue){
+    //         return total + currentvalue
+    //     },0) / arr.length)
+    // }
+
+    // console.log(average([8, 8 , 6.75])) // 8
+    // Ví dụ
+    /* 
+    Viết hàm positiveNumber để chuyển các số âm trong một mảng thành các số dương.
+    Yêu cầu sử dụng Math Object
+        [1,-2,-3,-4,5,6,7,-8] => [1,2,3,4,5,6,7,8]
+    // */
+    // function positiveNumber(arr) {
+    //   // your code here
+    //     return arr.map(function(item){
+    //         return Math.abs(item)
+    //     })
+    // }
+    // console.log(positiveNumber([1,-2,-3,-4,5,6,7,-8]))
+    // Ví dụ
+
+    /** 
+     * Viết hàm rightTriangle nhập vào 3 cạnh của một tam giác.
+     * Cho biết đó có phải là tam giác vuông hay không? 
+    */
+    // function rightTriangle(a, b, c) {
+    //   // your code here
+    //   if(Math.pow(a , 2) == (Math.pow(b , 2) + Math.pow(c , 2))){
+    //       return "day la tam giac vuong"
+    //   }else{
+    //       return "day khong phai la tam giac vuong"
+    //   }
+    // }
+    // console.log(rightTriangle(7,3,4))
+// 17 : Object handle
+// var cat = {
+//     name : 'Meo meo',
+//     weight : '3 kg',
+//     getWeight : function(){
+//         console.log(this.weight)
+//     }
 // }
-// console.log(powerup([1,3,2,5,6]))
-//Bai tap 2 
-// * 
-//   Dùng hàm average tính điểm trung bình 3 môn của một học sinh.
-//   Làm tròn điểm trung bình
+// constructor function
+// camelcase : quy luat dat ten bien
+// function Animal(name , weight){
+//     this.name = name
+//     this.weight = weight
+//     this.eat = function(food){
+//         console.log(food)
+//     }
+// }
+// var cat2 = new Animal("cat",2)
+// console.log(cat2.name)
+// console.log(cat2.weight)
+// cat2.boss = "Nguyen Van A"
+// console.log(cat2.boss)
+// **
+//  * Viết hàm countProperty để in ra độ dài của một đối tượng
+//  * @param {object}
+//  * @return {number}
+//  * 
+//  * Example: 
+//  * var student = {
+//  *      name: "Jerry", 
+//  *      age: 14,
+//  *      role: "student"
+//  *  }
+//  * lengthObj(student); // length: 3
+// }
 // */
 
-// function average(arr) {
-//     return Math.round(arr.reduce(function(total , currentvalue){
-//         return total + currentvalue
-//     },0) / arr.length)
-// }
-
-// console.log(average([8, 8 , 6.75])) // 8
-// Ví dụ
-/* 
-  Viết hàm positiveNumber để chuyển các số âm trong một mảng thành các số dương.
-  Yêu cầu sử dụng Math Object
-    [1,-2,-3,-4,5,6,7,-8] => [1,2,3,4,5,6,7,8]
-*/
-function positiveNumber(arr) {
-  // your code here
-    return arr.map(function(item){
-        return Math.abs(item)
-    })
+function countProperty(obj) {
+  // viết code ở đây
+  return Object.keys(obj).length
 }
-console.log(positiveNumber([1,-2,-3,-4,5,6,7,-8]))
+var student = {
+    name: "Jerry", 
+    age: 14,
+    role: "student"
+}
+console.log(countProperty(student))
