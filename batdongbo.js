e3e8b6dc968a2f2015eab27f98276169d4cd8278
phamtanphat <phatdroid94@gmail.com>
2019-11-22 23:09:11 -0800

    Fix bug promise

--- batdongbo.js
+++ batdongbo.js
@@ -42,10 +42,10 @@ function tru(a , b ){
     return new Promise((res , rej) => {
         const url = `https://pheptinhonline.herokuapp.com/tru/${a}/${b}`
         request(url , function (error , response , body) {
-            if(error) return rej(error.message);
+            if(error) return rej(error.message)
             const value = JSON.parse(body)
             if(!value.success) return rej(value.message)
-            return res(null , value.message)
+            return res(value.message)
         });
     })
 }
@@ -53,10 +53,10 @@ function nhan(a , b ){
     return new Promise((res , rej) => {
         const url = `https://pheptinhonline.herokuapp.com/nhan/${a}/${b}`
         request(url , function (error , response , body) {
-            if(error) return rej(error.message);
-            const value = JSON.rej(body)
+            if(error) return rej(error.message)
+            const value = JSON.parse(body)
             if(!value.success) return rej(value.message)
-            return res(null , value.message)
+            return res(value.message)
         });
     })
 }
@@ -64,10 +64,10 @@ function chia(a , b , cb){
     return new Promise((res , rej) => {
         const url = `https://pheptinhonline.herokuapp.com/chia/${a}/${b}`
         request(url , function (error , response , body) {
-            if(error) return rej(error.message);
+            if(error) return rej(error.message)
             const value = JSON.parse(body)
             if(!value.success) return rej(value.message)
-            return res(null , value.message)
+            return res(value.message)
         });
     })
 }
@@ -76,15 +76,18 @@ function chia(a , b , cb){
 //     console.log(body);
 // })
 // dien tich hinh chu nhat (s = a * b)
-// function dientich(a , b , cb){
-//     nhan(a , b , function(error,value){
-//         cb(error,value)
-//     })
-// }
-// dientich(10 , 5 , function(error , value){
-//     if(error) return console.log(error)
-//     return console.log(value)
-// })
+function dientich(a , b ){
+    return new Promise((res,rej) => {
+        nhan(a , b)
+        .then(tich => res(tich))
+        .catch(error => rej(error))
+    })
+}
+dientich( 5 , 6)
+.then(dientich => console.log(dientich))
+.catch(error => console.log(error))
+
+
 // chu vi hinh chu nhat(P = (A + B) X 2)
 // function chuvi( a , b , cb){
 //     cong(a , b , function(error , tong){
